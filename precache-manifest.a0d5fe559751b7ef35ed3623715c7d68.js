f972193af59291d54cc3476a7493b705c62e3f0a
Sudheer Singh <sudheersid0710@gmail.com>
2020-09-29 09:27:46 +0530

    Updates

---
+++ precache-manifest.a0d5fe559751b7ef35ed3623715c7d68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c932845d518eccd276a853cd471cc3",
+    "url": "/ToDo-WithReact/index.html"
+  },
+  {
+    "revision": "3b10a02ed088b2643139",
+    "url": "/ToDo-WithReact/static/css/main.0909134d.chunk.css"
+  },
+  {
+    "revision": "032936803bc28fc6c230",
+    "url": "/ToDo-WithReact/static/js/2.ed4e2313.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ToDo-WithReact/static/js/2.ed4e2313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b10a02ed088b2643139",
+    "url": "/ToDo-WithReact/static/js/main.8eda7620.chunk.js"
+  },
+  {
+    "revision": "dfebd4bccb7b8362ddde",
+    "url": "/ToDo-WithReact/static/js/runtime-main.2b032079.js"
+  },
+  {
+    "revision": "cb657af8b00c5521bcf6ec5d0ad311dd",
+    "url": "/ToDo-WithReact/static/media/team.cb657af8.png"
+  }
+]);
